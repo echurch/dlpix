bd206ceedae2a2d1ca8434c18b6cfbe6bab32df3
Eric Church <echurch@fnal.gov>
2018-01-02 20:43:03 +0000

    convert_example embellished to add saving of MCTruth info, which we want for kinematics studies

--- dlpix_wire/pytools/convert_example.py
+++ dlpix_wire/pytools/convert_example.py
@@ -2,4 +2,10 @@ from root_numpy import root2array, tree2array
 
 rfile = ROOT.TFile('/Users/chur558/dune-lbne/data/e-/singlepix_ana.root')
 intree = rfile.Get('largana/SDTTree')
+intree2 = rfile.Get('largana/MCTTree')
+
+
+# Below saves out the G4step electron channel-deposition info
 np.save("/Users/chur558/dune-lbne/data/e-/singlepix_ana_e-",array[1:10,])
+# Below saves out the MCTruth info 
+np.save("/Users/chur558/dune-lbne/data/e-/singlepix_truth_e-",array[1:10,])
