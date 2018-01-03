fcd0d112e33d760bfce9d482b186cd2bc968e228
Eric Church <echurch@fnal.gov>
2018-01-03 19:42:12 +0000

    README, requirements , clean-up

--- dlpix_wire/pytools/convert_example.py
+++ dlpix_wire/pytools/convert_example.py
@@ -1,19 +1,16 @@
 from ROOT import TFile
 from root_numpy import root2array, tree2array
+import numpy as np
 
 
 rfile = TFile('/Users/chur558/dune-lbne/data/e-/singlepix_ana.root')
 intree = rfile.Get('largana/SDTTree')
 intree2 = rfile.Get('largana/MCTTree')
-array = tree2array(intree)
-array2 = tree2array(intree2)
+array = tree2array(intree)    # takes 10s of seconds.
+array2 = tree2array(intree2,branches=['MCEvt','MCRun','MCSub','MCPdg','MCOrigin','MCMomentum'],selection='MCParentID==0')
 
-##### This involves pcl'ing, and doesn't work for python3 saving and python2.7 loading!
+##### This involves pcl'ing, and doesn't work for python3 saving and python2.7 loading or vice-versa, just FYI.
 # Below saves out the G4step electron channel-deposition info
-#np.save("/Users/chur558/dune-lbne/data/e-/singlepix_ana_e-",array[1:10,])
+np.save("/Users/chur558/dune-lbne/data/e-/singlepix_ana_e-",array[1:10,])
 # Below saves out the MCTruth info 
-#np.save("/Users/chur558/dune-lbne/data/e-/singlepix_truth_e-",array[1:10,])
-
-
-np.savetxt("/Users/chur558/dune-lbne/data/e-/singlepix_ana_e-.gz",array[1:10,])
-np.savetxt("/Users/chur558/dune-lbne/data/e-/singlepix_truth_e-.gz",array2[1:10,])
+np.save("/Users/chur558/dune-lbne/data/e-/singlepix_truth_e-",array2[1:10,])
